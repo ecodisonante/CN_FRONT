f2c07d0ae1357a077346ef5df3a64cfb9fbf5c41
yoryet <y.danoun@pharmatender.cl>
2025-02-16 22:04:10 -0300

    add routes medicos y pacientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,31 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
+import { DoctorManagementComponent } from './doctors/doctor-management/doctor-management.component';
+import { PatientManagementComponent } from './patients/patient-management/patient-management.component';
 
 export const routes: Routes = [
-
-    {
-        path: '',
-        loadComponent: () => import('./login/login.component').then(c => c.LoginComponent)
-    },
-    {
-        path: 'login',
-        loadComponent: () => import('./login/login.component').then(c => c.LoginComponent)
-    },
-    {
-        path: 'dashboard',
-        loadComponent: () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent),
-        canActivate: [AuthGuard]
-    }
-
+  {
+    path: '',
+    loadComponent: () => import('./login/login.component').then(c => c.LoginComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.component').then(c => c.LoginComponent)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent),
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'doctors',
+        component: DoctorManagementComponent
+      },
+      {
+        path: 'patients',
+        component: PatientManagementComponent
+      }
+    ]
+  }
 ];
