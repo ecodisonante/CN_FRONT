ff3799af9b8dd89026823224534c4d40e7f47e79
yoryet <y.danoun@pharmatender.cl>
2025-02-23 12:22:23 -0300

    feat : service measirement - enviroment production- dtomeasurement

---
+++ src/app/services/measurement.service.ts
@@ -0,0 +1,33 @@
+// src/app/services/measurement.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.prod';
+
+export interface MeasurementDTO {
+  id: number;
+  idPatient: number;
+  idSing: number;
+  measurementValue: number;
+  dateTime: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MeasurementService {
+  constructor(private http: HttpClient) {}
+
+  getMeasurementsByPatient(patientId: number): Observable<MeasurementDTO[]> {
+    
+    return this.http.get<MeasurementDTO[]>(`${environment.apiUrl}/measurement/getMeasurements/${patientId}`);
+  }
+
+  getLatestMeasurements(): Observable<MeasurementDTO[]> {
+    return this.http.get<MeasurementDTO[]>(`${environment.apiUrl}/kafka/measurements`);
+  }
+
+  saveMeasurement(measurement: Omit<MeasurementDTO, 'id'>): Observable<MeasurementDTO> {
+    return this.http.post<MeasurementDTO>(`${environment.apiUrl}/measurement/add`, measurement);
+  }
+}
